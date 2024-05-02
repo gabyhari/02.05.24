2b00c0fa70c0b84f96a5515bc6324cc37cc12af5
gabyhari <142436298+gabyhari@users.noreply.github.com>
2024-05-02 12:24:50 -0300

    Add files via upload

--- objeto.js
+++ objeto.js
@@ -1,15 +1,11 @@
 class Calebe {
     constructor (pinterest) {
-        this.site = site;
-        this.pinterest = pinterest;
+        this.site = pinterest;
     }
 
     exibirSite () {
-        window.open(this.site);
-        window.open(this.pinterest);
+        window.open(this.site)
     }
- }
-    var novoObjeto = new Objeto ("link do site que voce escolheu");
-
-    var novoCalebe = new Calebe ("https://br.pinterest.com/pin/580471839499221561/"); 
-    novoCalebe.exibirSite()
+}
+var novoCalebe = new Calebe ("https://br.pinterest.com/pin/13581236382167873/");
+novoCalebe.exibirSite();
